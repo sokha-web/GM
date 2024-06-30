87ae2124ccd61557e44b053fabf9d574b6ebdb72
Nika Sokhashvili <nika.sokhashvili.nika@gmail.com>
2024-06-30 06:36:41 +0400

    add

--- index.js
+++ index.js
@@ -13,3 +13,33 @@ closeButton.addEventListener("click", () => {
   sidebar.classList.remove("active");
   toggleButton.classList.remove("active"); // Optionally remove active class from toggle button
 });
+
+
+// Inline news
+
+const prevButton = document.getElementById('prev-button');
+const nextButton = document.getElementById('next-button');
+const sliderTrack = document.querySelector('.slider-track');
+let currentSlide = 0;
+
+function moveSlide(direction) {
+  if (direction === 'prev') {
+    currentSlide--;
+  } else {
+    currentSlide++;
+  }
+
+  currentSlide = Math.max(currentSlide, 0); // Don't go beyond first slide
+  currentSlide = Math.min(currentSlide, 5); // Don't go beyond last slide
+
+  sliderTrack.classList.remove('active'); // Remove active class for transition
+  sliderTrack.offsetWidth; // Force reflow to ensure smooth transition
+  sliderTrack.classList.add('active'); // Add active class for transition
+
+  sliderTrack.style.transform = `translateX(-${currentSlide * (100 / 6)}%)`; // Adjust based on slide width calculation
+}
+
+prevButton.addEventListener('click', () => moveSlide('prev'));
+nextButton.addEventListener('click', () => moveSlide('next'));
+
+
